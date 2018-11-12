02e26d6a12d2ccf712cc599bb54dece7c979023c
Radif <32257685+JustxNick@users.noreply.github.com>
2018-11-12 12:30:33 +0300

    Flappy Bird v1.0

---
+++ flappybird/js/game.js
@@ -0,0 +1,73 @@
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeUp = new Image();
+var pipeBottom = new Image();
+
+bird.src = "img/flappy_bird_bird.png";
+bg.src = "img/flappy_bird_bg.png";
+fg.src = "img/flappy_bird_fg.png";
+pipeUp.src = "img/flappy_bird_pipeUp.png";
+pipeBottom.src = "img/flappy_bird_pipeBottom.png";
+
+var gap = 90;
+
+document.addEventListener("mousedown", moveUp);
+
+function moveUp() {
+    yPos -= 27;
+}
+
+var pipe = [];
+
+pipe[0] = {
+    x : cvs.width,
+    y : 0
+}
+
+var score = 0;
+
+var xPos = 10;
+var yPos = 150;
+var grav = 1.5;
+
+
+function draw() {
+    ctx.drawImage(bg, 0, 0);
+
+    for (var i = 0; i < pipe.length; i++) {
+        ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+        ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+    
+        pipe[i].x--;
+        if (pipe[i].x == 125) {
+            pipe.push({
+                x : cvs.width,
+                y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+            });
+        }
+
+        if (xPos + bird.width >= pipe[i].x && xPos <= pipe[i].x + pipeUp.width && (yPos <= pipe[i].y + pipeUp.height || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height || yPos + bird.height >= cvs.height) {
+            location.reload();
+        }
+
+        if (pipe[i].x == 5) {
+            score++;
+        }
+    }
+
+    ctx.drawImage(fg, 0, cvs.height - fg.height);
+    ctx.drawImage(bird, xPos, yPos);
+
+    yPos += grav;
+
+    ctx.fillStyle = "#000";
+    ctx.font = "20px Verdana";
+    ctx.fillText("Score: " + score, 10, cvs.height - 20);
+    requestAnimationFrame(draw);
+}
+
+pipeBottom.onload = draw;
